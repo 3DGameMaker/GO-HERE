97eaf48ff7ad4afb4555a9a7a2fb3acb53c14c20
3DEngineer <97719456+3DGameMaker@users.noreply.github.com>
2023-10-04 17:02:24 +1300

    Update CLICK HERE PLEASE.sh

--- CLICK HERE PLEASE.sh
+++ CLICK HERE PLEASE.sh
@@ -1,4 +1,4 @@
 echo When you get the progam, before saying its broken, double click.
 echo Also, this is the oldest version possible so it was still probably beta. Don't blame me.
     
-read -s -n 1
+read -n 1 -r -s -p $'Press enter to continue...\n'
