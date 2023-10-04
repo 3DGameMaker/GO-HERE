81aed9bd149d3b3b5cc086073a9dbf11bfe82874
3DEngineer <97719456+3DGameMaker@users.noreply.github.com>
2023-10-04 17:00:30 +1300

    Update CLICK HERE PLEASE.sh

--- CLICK HERE PLEASE.sh
+++ CLICK HERE PLEASE.sh
@@ -1,4 +1,4 @@
 echo When you get the progam, before saying its broken, double click.
 echo Also, this is the oldest version possible so it was still probably beta. Don't blame me.
     
-read junk
+pause
