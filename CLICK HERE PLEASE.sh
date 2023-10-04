e7b6c1cfe152c0e63825c8b6c02f59866478ff54
3DEngineer <97719456+3DGameMaker@users.noreply.github.com>
2023-10-04 17:11:40 +1300

    Update CLICK HERE PLEASE.sh

--- CLICK HERE PLEASE.sh
+++ CLICK HERE PLEASE.sh
@@ -1,4 +1,20 @@
-echo When you get the progam, before saying its broken, double click.
-echo Also, this is the oldest version possible so it was still probably beta. Don't blame me.
-    
-read -n 1 -r -s -p $'Press enter to continue...\n'
+echo "When you get the progam, before saying its broken, double click."
+
+echo "Also, this is the oldest version possible so it was still probably beta. Don't blame me."
+ 
+# Wait for the user to press a key
+read -s -n 1 key
+ 
+# Check which key was pressed
+case $key in
+    y|Y)
+        echo "You pressed 'y'. Continuing..."
+        ;;
+    n|N)
+        echo "You pressed 'n'. Exiting..."
+        exit 1
+        ;;
+    *)
+        echo "Invalid input. Please press 'y' or 'n'."
+        ;;
+esac
