9b0efe5ad86cf03e88bc9619d076bfabec2a7414
3DEngineer <97719456+3DGameMaker@users.noreply.github.com>
2023-10-04 16:55:28 +1300

    Update CLICK HERE PLEASE.sh

--- CLICK HERE PLEASE.sh
+++ CLICK HERE PLEASE.sh
@@ -1,6 +1,8 @@
+echo When you get the progam, before saying its broken, double click.
+echo Also, this is the oldest version possible so it was still probably beta. Don't blame me.
+    
 read -n1 -s -r -p $'Press space to continue...\n' key
 
 if [ "$key" = ' ' ]; then
-    echo When you get the progam, before saying its broken, double click.
-    echo Also, this is the oldest version possible so it was still probably beta. Don't blame me.
+
 fi
