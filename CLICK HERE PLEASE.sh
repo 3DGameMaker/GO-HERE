e9c2848af83c7a83398e0b947be36f546eab9605
3DEngineer <97719456+3DGameMaker@users.noreply.github.com>
2023-10-04 17:17:11 +1300

    Update CLICK HERE PLEASE.sh

--- CLICK HERE PLEASE.sh
+++ CLICK HERE PLEASE.sh
@@ -1,6 +1,4 @@
-echo "When you get the progam, before saying its broken, double click."
-
-echo "Also, this is the oldest version possible so it was still probably beta. Don't blame me."
+echo "This is the oldest version possible so it was still probably beta."
  
 # Wait for the user to press a key
 read -s -n 1 key
